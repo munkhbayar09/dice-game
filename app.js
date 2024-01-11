f4b27db142755d933b0cd267908edd78db37ea95
muugii <ba.bayaraa09@gmail.com>
2024-01-11 12:20:39 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -9,19 +9,29 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // shoonii al taliig buusniig hadgalah huvi, 1-6 utga randomoor
-var dice = Math.floor (Math.random () * 6) + 1;
+var diceNumber = Math.floor (Math.random () * 6) + 1;
 
 // div class="player-score" id="score-0">43</div>
 // window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").textContent = dice;
 
-// программ эхлэхэд бэлдэх
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+// программ эхлэхэд бэлтгэх
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = '0';
+
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(" .dice")
 
 document.querySelector(" .dice").style.display = "none";
 
-console.log("shoo :" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+      var diceNumber = Math.floor (Math.random () * 6) + 1;
+      diceDom.style.display = "block";
+      diceDom.src = "dice-" + diceNumber + ".png";
+      
+
+      alert("shoo buulaa :" + diceNumber);
+});
